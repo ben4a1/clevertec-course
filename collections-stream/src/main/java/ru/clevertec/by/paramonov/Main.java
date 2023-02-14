656b4db4c54f04edcd3371253951991faedc8f67
Alexey Paramonov <paramonov_tama@mail.ru>
2023-02-14 22:09:45 +0300

    nothing: intermediate commit

--- collections-stream/src/main/java/ru/clevertec/by/paramonov/Main.java
+++ collections-stream/src/main/java/ru/clevertec/by/paramonov/Main.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) throws IOException {
 //        task1();
-        task2();
+//        task2();
 //        task3();
 //        task4();
 //        task5();
