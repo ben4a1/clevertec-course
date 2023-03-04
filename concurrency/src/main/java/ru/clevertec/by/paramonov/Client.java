e87f5cacb7d7d46618d398dbf80cbf52fd1eda68
Alexey Paramonov <paramonov_tama@mail.ru>
2023-03-04 21:38:28 +0300

    refactor: sendRequestList() - with Callable and Future solution

--- concurrency/src/main/java/ru/clevertec/by/paramonov/Client.java
+++ concurrency/src/main/java/ru/clevertec/by/paramonov/Client.java
@@ -5,8 +5,7 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -29,19 +28,21 @@ public class Client {
     }
 
     public void sendRequestList() {
-        requestList.forEach(request -> executorService.submit(() -> {
-            int requestIntValue = request.getIntValue();
-            System.out.println("Client sending request with Value: " + requestIntValue + ". Current thread " + Thread.currentThread().getName());
-            Response calculate = server.calculate(request);
-            if (calculate != null) {
-                int responseIntValue = calculate.getIntValue();
-                System.out.println("Client getting response with Value: " + responseIntValue + ". Current thread " + Thread.currentThread().getName());
-                lock.lock();
-                responseList.add(calculate);
-                lock.unlock();
-            }
-        }));
+        List<Callable<Response>> callableListOfResponse = requestList.stream().map(request -> (Callable<Response>)() -> server.calculate(request)).toList();
+        try {
+            List<Future<Response>> futureList = executorService.invokeAll(callableListOfResponse);
+            futureList.forEach(responseFuture -> {
+                try {
+                    lock.lock();
+                    responseList.add(responseFuture.get());
+                    lock.unlock();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         executorService.shutdown();
     }
-
 }
--- concurrency/src/main/java/ru/clevertec/by/paramonov/Runner.java
+++ concurrency/src/main/java/ru/clevertec/by/paramonov/Runner.java
@@ -1,16 +1,12 @@
 package ru.clevertec.by.paramonov;
 
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Runner {
     private static final int START_MAX_LIST_SIZE = 20;
     private static final Random RANDOM = new Random();
     public static void main(String[] args) {
         Client client = new Client();
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        executorService.submit(client::sendRequestList);
-        executorService.shutdown();
+        client.sendRequestList();
     }
 }
