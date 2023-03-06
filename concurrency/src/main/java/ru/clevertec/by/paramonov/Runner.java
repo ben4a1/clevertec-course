0a168ebab2a191e74c9df3b1eb26e6effb1dd071
Alexey Paramonov <paramonov_tama@mail.ru>
2023-03-06 10:21:06 +0300

    remove unnecessary lines in runner.class

--- concurrency/src/main/java/ru/clevertec/by/paramonov/Runner.java
+++ concurrency/src/main/java/ru/clevertec/by/paramonov/Runner.java
@@ -1,10 +1,6 @@
 package ru.clevertec.by.paramonov;
 
-import java.util.Random;
-
 public class Runner {
-    private static final int START_MAX_LIST_SIZE = 20;
-    private static final Random RANDOM = new Random();
     public static void main(String[] args) {
         Client client = new Client();
         client.sendRequestList();
